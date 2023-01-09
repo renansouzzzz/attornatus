094483fe0b941696badbdc7febd27ad04783d7ad
renansouzzzz <renansouza.oliv@outlook.com>
2023-01-08 21:23:28 -0300

    teste junit done

--- src/main/java/br/com/eloware/apirest/services/PessoaServico.java
+++ src/main/java/br/com/eloware/apirest/services/PessoaServico.java
@@ -2,6 +2,7 @@ package br.com.eloware.apirest.services;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,52 +15,35 @@ public class PessoaServico {
 	
 	@Autowired
 	private PessoaRepository repository;
-	
+	@Test
 	public Pessoa editarPessoa(Long id, Pessoa pessoa) {
-		try 
-		{
-			Pessoa novaPessoa = repository.findById(id).get();
-			novaPessoa.setNome(pessoa.getNome());
-			novaPessoa.setDataNascimento(pessoa.getDataNascimento());
-			novaPessoa.setEndereco(pessoa.getEndereco());
-			repository.save(novaPessoa);
-		} 
 		
-		catch (IllegalArgumentException e) 
-		{
-			System.out.println(e);
-		}
-		return pessoa;
-		
-		
+		Pessoa novaPessoa = repository.findById(id).get();
+		novaPessoa.setNome(pessoa.getNome());
+		novaPessoa.setDataNascimento(pessoa.getDataNascimento());
+		novaPessoa.setEndereco(pessoa.getEndereco());
+		repository.save(novaPessoa);
+			return pessoa;
 	}
-	
+	@Test
 	public Object criarEndereco(Long id, Pessoa pessoa) {
-		try 
-		{
-			Pessoa novaPessoa = repository.findById(id).get();
-			novaPessoa.setEndereco(pessoa.getEndereco());
-			repository.save(novaPessoa);
-		} 
 		
-		catch (IllegalArgumentException e) 
-		{
-			System.out.println(e);
-		}
-		return pessoa;
-		
+		Pessoa novaPessoa = repository.findById(id).get();
+		novaPessoa.setEndereco(pessoa.getEndereco());
+		repository.save(novaPessoa);
+			return pessoa;
 	}
-	
+	@Test
 	public List<Endereco> listarEnderecos(Long id) {
+		
 		Pessoa pessoa = repository.findById(id).get();
-		return pessoa.getEndereco();
+			return pessoa.getEndereco();
 	}
-	
+	@Test
 	public Endereco informarEnderecoPrincipal(Long id) {
+			
 		Pessoa pessoa = repository.findById(id).get();
 		Endereco enderecoPrincipal = pessoa.getEndereco().get(0);
-		return enderecoPrincipal;
+			return enderecoPrincipal;
 	}
-	
-	
 }
